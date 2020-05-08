0a5876f78886fe1c614c747c3990287ad73147cf
Ramaraj Thangapandi <ramaraj_thangapandi@trimble.com>
2020-05-08 21:41:06 +0530

    parse the individual icons and draw them on a canvas to create sprite image

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var fs = require('fs');
 
 var AdmZip = require('adm-zip');
 
+const { createCanvas, loadImage } = require('canvas')
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -17,12 +19,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 app.use(fileUpload());
 
 
 app.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 app.post("/upload", function (req, res) {
@@ -52,8 +54,31 @@ app.post("/upload", function (req, res) {
             let unzippedFolder = path.join(folder, "unzipped");
             mkdirp(unzippedFolder, null);
             var zip = new AdmZip(filePath);
+            var zipEntries = zip.getEntries();
             zip.extractAllTo(unzippedFolder, true);
 
+            let width = 40;
+            let height = 20;
+            const canvas = createCanvas(width * 27, height * 27);
+            const ctx = canvas.getContext('2d');            
+
+            zipEntries.forEach(function(zipEntry) {
+                let position = 1;
+                loadImage(path.join(unzippedFolder, zipEntry.name)).then((image) => {
+                    let name = zipEntry.name.replace(".png", "");
+                    let xPosition = name.charCodeAt(0) - 96;
+                    let yPosition = name.charCodeAt(1) - 96;
+                    ctx.drawImage(image, xPosition * width, yPosition * height, width, height)
+                    position ++ ;
+                });
+            });
+
+            loadImage(path.join(unzippedFolder, zipEntries[0].name)).then((image) => {
+                const out = fs.createWriteStream(path.join(folder, "flags-sprite.png"))
+                const stream = canvas.createPNGStream()
+                stream.pipe(out)
+                out.on('finish', () => console.log('The PNG file was created.'))                
+            })
             
         });
     });
